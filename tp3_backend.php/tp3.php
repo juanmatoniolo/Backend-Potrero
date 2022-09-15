8e848b9ae55227ad763771dba06ecc1a99915352
JunmaToniolo <juanmatoniolo2@gmail.com>
2022-09-15 02:27:55 -0300

    tp3- login

--- tp3_backend.php/tp3.php
+++ tp3_backend.php/tp3.php
@@ -1,18 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>tp3 de php</title>
-    <link rel="stylesheet" href="estilo.css">
+    
 
 </head>
+
 <body>
-    
+    <?php
 
-    
+    //Vinculamos el loing con php 
+    $usuario =  $_POST["usuario"];
+    $pass  = $_POST["pass"];
 
+    //Creamos las variables para validar usuario y contraseña 
 
+    $ckusuario = "usuario";
+    $ckpass = "pass";
+
+    //Creamos un IF para validacion de datos: 
+    if ($usuario == $ckusuario && $pass == $ckpass) {
+        //echo "Correcto!"; 
+        header("location:./principal.html");
+    } else { 
+
+        header("location:./login.html");
+        
+    }
+
+    ?>
+   
 </body>
+
 </html>
