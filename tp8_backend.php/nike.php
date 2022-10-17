eddb17b1bbe662ae3d40b4f3a928062814dcb7a6
JunmaToniolo <juanmatoniolo2@gmail.com>
2022-10-17 14:30:03 -0300

    tp-8

---
+++ tp8_backend.php/nike.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>listar</title>
+</head>
+
+<body>
+    <h1>Tienda de Ropa</h1>
+
+    <button type="submit"><a href="./index.html">Index</a> </button>
+    <button type="submit"><a href="./listar.php">Listar</a> </button>
+    <button type="submit"><a href="./buzo.php">Buzos</a> </button>
+    <button type="submit"><a href="./nike.php">nike</a> </button>
+    <button type="submit"><a href="./mayores_a_500.php">mayores a 500</a> </button>
+
+
+    <table style="text-align: center" border="1" >
+        <tr>
+            <th>ID</th>
+            <th>TIP DE PRENDA</th>
+            <th>MARCA</th>
+            <th>TALLE</th>
+            <th>PRECIO</th>
+        </tr>
+
+
+        <?php
+
+        //Conexion
+        $conexion = mysqli_connect("127.0.0.1", "root", "");
+
+        // conexion , nombre de la base de dato -- esto siemre es igual 
+        mysqli_select_db($conexion, "tienda");
+
+        // Preparar la orden SQL
+
+        $consulta = "SELECT*FROM ropa WHERE marca = 'nike'";
+
+
+        //Ejecutar la orden y obtener datos
+
+        $datos = mysqli_query($conexion, $consulta);
+
+
+        // Ir Imprimiendo las filas resultantes
+
+
+
+        while ($fila = mysqli_fetch_array($datos)) {
+        ?>
+            <tr>
+                <td><?php echo $fila['id']; ?> </td>
+                <td><?php echo $fila['ropa']; ?> </td>
+                <td><?php echo $fila['marca']; ?> </td>
+                <td><?php echo $fila['talle']; ?> </td>
+                <td><?php echo $fila['precio']; ?> </td>
+            </tr>
+        <?php } ?>
+    </table>
+
+
+</body>
+
+</html>
